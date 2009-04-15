aa3ff6d78b474dd93c4d2d446dbddbf9ce258141
Patrick Oosterveld <patrick.oosterveld@gmail.com>
2009-04-15 20:44:07 +0000

    copy utils bugfix
    git-svn-id: http://cakupan.googlecode.com/svn/trunk/cakupan@47 82d3c26e-0f4d-11de-a22a-7557bf72ca44

--- src/com/cakupan/xslt/report/HTMLCoverageReport.java
+++ src/com/cakupan/xslt/report/HTMLCoverageReport.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.util.Map;
 
 import javax.xml.transform.stream.StreamSource;
